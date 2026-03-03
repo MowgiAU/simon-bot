79acf1e5a50cefb7e4c341e4ebdb86c62b02cd92
MowgiAU <thesnowau@gmail.com>
2026-03-03 23:28:05 +1030

    Fix discovery page crash (Wrap discover entry in BrowserRouter)

--- dashboard/src/discover.tsx
+++ dashboard/src/discover.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { ArtistDiscoveryPage } from './pages/ArtistDiscovery';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <div className="app">
-      <ArtistDiscoveryPage />
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <ArtistDiscoveryPage />
+      </div>
+    </BrowserRouter>
   </React.StrictMode>
 );
