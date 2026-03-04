db8685cb59cf45de9d4503ea10512871a1c24359
MowgiAU <thesnowau@gmail.com>
2026-03-04 22:38:07 +1030

    fix: restore PlayerProvider and providers to resolve usePlayer error

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -450,11 +450,15 @@ const AppContent: React.FC = () => {
 export const App: React.FC = () => {
   console.log('[App] Mounting App Root');
   return (
-    <div id="emergency-render" style={{ background: 'darkblue', color: 'white', padding: '100px', fontSize: '30px' }}>
-      APP ROOT IS RENDERING BUT PROVIDERS ARE REMOVED
-      <hr />
-      <AppContent />
-    </div>
+    <ErrorBoundary>
+      <PlayerProvider>
+        <style>{AppStyles}</style>
+        <div id="app-structure">
+          <AppContent />
+          <GlobalPlayer />
+        </div>
+      </PlayerProvider>
+    </ErrorBoundary>
   );
 };
 
