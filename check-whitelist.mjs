ded04fbb48051500d2021419c689a8f1007d2857
MowgiAU <thesnowau@gmail.com>
2026-04-13 16:06:54 +0930

    cleanup: remove debug script

--- check-whitelist.mjs
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const db = new PrismaClient();
-const all = await db.welcomeGateSettings.findMany();
-for (const s of all) {
-  console.log(`Guild ${s.guildId}: whitelistedChannelIds=${JSON.stringify(s.whitelistedChannelIds)} (${s.whitelistedChannelIds?.length})`);
-}
-await db.$disconnect();
